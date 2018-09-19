ff4e1e16c5b2d11b597c3fb462728eac46553942
Oliver <oliverlh@uio.no>
2018-09-19 18:50:08 +0200

    det gaar ike

--- project2/project2.py
+++ project2/project2.py
@@ -6,6 +6,7 @@ n = 10
 
 a = -np.ones(n-1)
 d = 2*np.ones(n)
+d[0] += 1E-15
 
 """
 d[0]=random.randint(0,10)
@@ -16,15 +17,16 @@ for i in range(n):
 #print d, a
 
 A=1
-eps = 1E-10
+eps = 1E-15
 while A>eps:
+    print A
     m = np.argmax(abs(a))
     #print a[m]
-    theta = (np.pi/4.0)-0.5*np.arctan((d[m+1]-d[m])/(2.0*a[m]))
+    theta = 0.5*np.arctan(2*a[m]/(d[m+1]-d[m]))
     s=np.sin(theta)
     c=np.cos(theta)
-    x=d[m]*c**2   +2.0*a[m]*c*s + d[m+1]*s**2
-    y=d[m+1]*c**2 +2.0*a[m]*c*s + d[m]*s**2
+    x=d[m]*c**2   - 2.0*a[m]*c*s + d[m+1]*s**2
+    y=d[m+1]*c**2 + 2.0*a[m]*c*s + d[m]*s**2
     a[m]=(d[m] - d[m+1])*c*s + a[m]*(c**2 - s**2)
     d[m] = x
     d[m+1] = y
@@ -33,7 +35,7 @@ while A>eps:
         z+=a[i]**2
     A=np.sqrt(z)
 
-#print d, a
+
 
 m1 = np.reshape(np.zeros(n**2),(n,n))
 
@@ -45,4 +47,27 @@ for i in range(n):
             m1[i,j] = -1.
         elif i-1 == j:
             m1[i,j] = -1.
-print np.linalg.eigvalsh(m1)
+
+"""
+A=1.
+while A>eps:
+    m = np.argmax(abs(m1))
+    print m
+
+
+    theta = (np.pi/4.0)-0.5*np.arctan((m1[m][m]-m1[m+1][m+1])/(2.0*a[m]))
+    s=np.sin(theta)
+    c=np.cos(theta)
+    x=d[m+1]*c**2   - 2.0*a[m]*c*s + d[m]*s**2
+    y=d[m]*c**2 + 2.0*a[m]*c*s + d[m+1]*s**2
+    a[m]=(d[m+1] - d[m])*c*s + a[m]*(c**2 - s**2)
+    d[m+1] = x
+    d[m] = y
+
+    z=0.
+    for i in range(len()):
+        z+=a[i]**2
+    A=np.sqrt(z)
+"""
+for i in range(len(d)):
+    print d[i], "           ", np.linalg.eigvalsh(m1)[i]
