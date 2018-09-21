c50391440f6c9ca1fcb1bb8f4a69834260ff4ee9
Oliver <oliverlh@uio.no>
2018-09-21 17:00:11 +0200

    1b

--- project2/project2.py
+++ project2/project2.py
@@ -3,40 +3,12 @@ import math
 import random
 
 n = 10
-
-a = -np.ones(n-1)
-d = 2*np.ones(n)
-d[0] += 1E-15
+h = 1./n #
+A=1.
 
 """
-d[0]=random.randint(0,10)
-for i in range(n):
-    a[i] = random.randint(0,10)
-    d[i+1] = random.randint(0,10)
+Making a matrix and finding eigenvalues with lib func.
 """
-#print d, a
-
-A=1
-eps = 1E-15
-while A>eps:
-    print A
-    m = np.argmax(abs(a))
-    #print a[m]
-    theta = 0.5*np.arctan(2*a[m]/(d[m+1]-d[m]))
-    s=np.sin(theta)
-    c=np.cos(theta)
-    x=d[m]*c**2   - 2.0*a[m]*c*s + d[m+1]*s**2
-    y=d[m+1]*c**2 + 2.0*a[m]*c*s + d[m]*s**2
-    a[m]=(d[m] - d[m+1])*c*s + a[m]*(c**2 - s**2)
-    d[m] = x
-    d[m+1] = y
-    z=0.
-    for i in range(len(a)):
-        z+=a[i]**2
-    A=np.sqrt(z)
-
-
-
 m1 = np.reshape(np.zeros(n**2),(n,n))
 
 for i in range(n):
@@ -47,27 +19,72 @@ for i in range(n):
             m1[i,j] = -1.
         elif i-1 == j:
             m1[i,j] = -1.
+m1/=(h**2)
+m2 = m1.copy() #controlling eigenvalues at the end
 
 """
-A=1.
+Finding indexes with max value
+"""
+def maksoffdiag(matrise, n, max=0,q=0,p=0):
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                aij = np.abs(matrise[i][j])
+                if aij > max:
+                    max = aij
+                    p = i
+                    q = j
+    return p, q
+
+"""
+Jacobi's method
+"""
+eps = 1E-4
 while A>eps:
-    m = np.argmax(abs(m1))
-    print m
 
+    l, k = maksoffdiag(m1,n)
+    tau = (m1[l][l]-m1[k][k])/(2.*m1[k][l])
 
-    theta = (np.pi/4.0)-0.5*np.arctan((m1[m][m]-m1[m+1][m+1])/(2.0*a[m]))
-    s=np.sin(theta)
-    c=np.cos(theta)
-    x=d[m+1]*c**2   - 2.0*a[m]*c*s + d[m]*s**2
-    y=d[m]*c**2 + 2.0*a[m]*c*s + d[m+1]*s**2
-    a[m]=(d[m+1] - d[m])*c*s + a[m]*(c**2 - s**2)
-    d[m+1] = x
-    d[m] = y
+    if tau >= 0: #matinf1100 curriculum before mid-term
+        t = ( -tau -np.sqrt(1+tau*tau))
+    else:
+        t = (-tau +np.sqrt(1+tau*tau))
+
+    c = 1./(np.sqrt(1+t*t))
+    s = c*t
+    for i in range(n): #
+        if i != k and i != l:
+                ik = m1[i][k]
+                il = m1[i][l]
+                m1[i][k] = ik*c - il*s
+                m1[i][l] = il*c + ik*s
+                m1[k][i] = m1[i][k] #symmetric matrix
+                m1[l][i] = m1[i][l]
+
+    x=m1[k][k]*c**2 - 2.0*m1[k][l]*c*s + m1[l][l]*s**2
+    y=m1[l][l]*c**2 + 2.0*m1[k][l]*c*s + m1[k][k]*s**2
+
+    m1[k][l]= 0. #(m1[k][k] - m1[l][l])*c*s + m1[k][l]*(c**2 - s**2)
+    m1[l][k] = m1[k][l] #symmetric matrix
+
+    m1[k][k] = x
+    m1[l][l] = y
 
     z=0.
-    for i in range(len()):
-        z+=a[i]**2
+
+    #Frobenius norm
+    for u in range(n):
+        for v in range(n):
+            if u != v:
+                z+=(m1[u][v])**2
     A=np.sqrt(z)
+
 """
-for i in range(len(d)):
-    print d[i], "           ", np.linalg.eigvalsh(m1)[i]
+printing eigenvalues.
+"""
+
+for i in range(n):
+    print "computed values = ",m1[i][i]
+x = np.linalg.eigvalsh(m2)
+for i in range(len(x)):
+    print "exact values = ", x[i]
