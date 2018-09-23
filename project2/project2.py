db5adc914e603f16fb8158c7daaf0e7c1c0390d7
Oliver <oliverlh@uio.no>
2018-09-23 19:23:44 +0200

    stunning definitions with a defined number of similarity transformations

--- project2/project2.py
+++ project2/project2.py
@@ -1,26 +1,28 @@
 import numpy as np
 import math
 import random
+import matplotlib.pyplot as plt
 
-n = 10
-h = 1./n #
-A=1.
 
 """
 Making a matrix and finding eigenvalues with lib func.
 """
-m1 = np.reshape(np.zeros(n**2),(n,n))
 
-for i in range(n):
-    for j in range(n):
-        if i==j:
-            m1[i,i] = 2.
-        elif i+1 == j:
-            m1[i,j] = -1.
-        elif i-1 == j:
-            m1[i,j] = -1.
-m1/=(h**2)
-m2 = m1.copy() #controlling eigenvalues at the end
+def tridiag(n):
+    m1 = np.reshape(np.zeros(n**2),(n,n))
+
+    for i in range(n):
+        for j in range(n):
+            if i==j:
+                m1[i,i] = 2.
+            elif i+1 == j:
+                m1[i,j] = -1.
+            elif i-1 == j:
+                m1[i,j] = -1.
+    h = 1./n #
+    m1 /= (h**2)
+    m2  = m1.copy() #controlling eigenvalues at the end
+    return m1
 
 """
 Finding indices with max value
@@ -39,21 +41,23 @@ def maksoffdiag(matrise, n, max=0,q=0,p=0):
 """
 Jacobi's method
 """
-eps = 1E-4
-while A>eps:
 
-    l, k = maksoffdiag(m1,n)
-    tau = (m1[l][l]-m1[k][k])/(2.*m1[k][l])
+def jacobi(m1, n, A=1., antall=0, eps = 1E-8):
 
-    if tau >= 0: #matinf1100 curriculum before mid-term
-        t = ( -tau -np.sqrt(1+tau*tau))
-    else:
-        t = (-tau +np.sqrt(1+tau*tau))
+    while A>eps:
 
-    c = 1./(np.sqrt(1+t*t))
-    s = c*t
-    for i in range(n): #
-        if i != k and i != l:
+        l, k = maksoffdiag(m1,n)
+        tau = (m1[l][l]-m1[k][k])/(2.*m1[k][l])
+
+        if tau >= 0: #matinf1100 curriculum before mid-term
+            t = ( -tau -np.sqrt(1+tau*tau))
+        else:
+            t = (-tau +np.sqrt(1+tau*tau))
+
+        c = 1./(np.sqrt(1+t*t))
+        s = c*t
+        for i in range(n): #
+            if i != k and i != l:
                 ik = m1[i][k]
                 il = m1[i][l]
                 m1[i][k] = ik*c - il*s
@@ -61,30 +65,60 @@ while A>eps:
                 m1[k][i] = m1[i][k] #symmetric matrix
                 m1[l][i] = m1[i][l]
 
-    x=m1[k][k]*c**2 - 2.0*m1[k][l]*c*s + m1[l][l]*s**2
-    y=m1[l][l]*c**2 + 2.0*m1[k][l]*c*s + m1[k][k]*s**2
+        x=m1[k][k]*c**2 - 2.0*m1[k][l]*c*s + m1[l][l]*s**2
+        y=m1[l][l]*c**2 + 2.0*m1[k][l]*c*s + m1[k][k]*s**2
 
-    m1[k][l]= 0. #(m1[k][k] - m1[l][l])*c*s + m1[k][l]*(c**2 - s**2)
-    m1[l][k] = m1[k][l] #symmetric matrix
+        m1[k][l]= 0. #(m1[k][k] - m1[l][l])*c*s + m1[k][l]*(c**2 - s**2)
+        m1[l][k] = m1[k][l] #symmetric matrix
 
-    m1[k][k] = x
-    m1[l][l] = y
+        m1[k][k] = x
+        m1[l][l] = y
 
-    z=0.
+        z=0.
 
     #Frobenius norm
-    for u in range(n):
-        for v in range(n):
-            if u != v:
-                z+=(m1[u][v])**2
-    A=np.sqrt(z)
+        for u in range(n):
+            for v in range(n):
+                if u != v:
+                    z+=(m1[u][v])**2
+        A=np.sqrt(z)
+
+        antall += 1 #how many similarity transformation needed
+    return m1, antall
 
 """
-printing eigenvalues.
+Estimating the number of transformations needed.
 """
+
+def transformation(m):
+    x = []
+    y = []
+    for i in range(m):
+        matrix, antall = jacobi(tridiag(i+1),i+1)
+        x.append(antall)
+        y.append(i+1)
+
+    plt.plot(y,x)
+    plt.xlabel("Dimensionality")
+    plt.ylabel("Number of transformations")
+    plt.title("Plot of similarity transformation")
+    plt.show()
+    #plt.savefig("numberoftrans.png")
+n=10
+transformation(n)
+matrix, antall = jacobi(tridiag(n), n)
+
+"""
+comparing the results with a function from numpy
+"""
+
 
 for i in range(n):
-    print "computed values = ",m1[i][i]
+    print "eigenvalues = ", matrix[i][i]
+
+"""
 x = np.linalg.eigvalsh(m2)
+
 for i in range(len(x)):
-    print "exact values = ", x[i]
+    print "from numpy: eigenvalues = ", x[i]
+"""
