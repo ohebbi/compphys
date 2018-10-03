1ed27c20428ab6dc74c09e00ac2caf8687cb54a2
Ingvild Bergsbak <ingbergs@uio.no>
2018-10-03 16:24:29 +0200

    values

--- project3/taska.cpp
+++ project3/taska.cpp
@@ -3,21 +3,24 @@
 #include <math.h>
 #include <string.h>
 #include <stdio.h>
+#include<fstream>
 
 using namespace std;
 
 int main(int argc,char* argv[]){
-    int n = 10;
-  
-    vector<double> vx(n, 0);
-    vector<double> vy(n, 0);
-    vector<double> rx(n, 0);
-    vector<double> ry(n, 0);
+    int n = 10000;
 
-    vx[0]=(1/sqrt(2));
-    vy[0]=(1/sqrt(2));
+    ofstream myfile;
+  
+    vector<double> vx(n, 1);
+    vector<double> vy(n, 1);
+    vector<double> rx(n, 1);
+    vector<double> ry(n, 1);
+
+    vx[0]=(-1.136);
+    vy[0]=(6.172);
     rx[0]=(1);
-    ry[0]=(0);
+    ry[0]=(0.0);
 
     
     
@@ -26,43 +29,51 @@ int main(int argc,char* argv[]){
     if ( strncmp(argv[1], "e", 2)==0){
       cout << "euler" << endl;
         for(int i = 1; i < n; i++){
-            float r = sqrt(pow(rx[i-1],2)+pow(ry[i-1],2));
-
-            vx[i] = vx[i-1]+h*4.0*pow(3.14159, 2.0)*rx[i-1]/pow(r,3.0);
+	  float r = sqrt(pow(rx[i-1],2)+pow(ry[i-1],2));
+	  //float r = 1.0;
+            vx[i] = vx[i-1]-h*4.0*pow(M_PI, 2.0)*rx[i-1]/pow(r,3.0);
             rx[i] = rx[i-1]+h*vx[i-1];
 
-            vy[i] = vy[i-1]+h*4.0*pow(3.14159, 2.0)*ry[i-1]/pow(r,3.0);
+            vy[i] = vy[i-1]-h*4.0*pow(M_PI, 2.0)*ry[i-1]/pow(r,3.0);
             ry[i] = ry[i-1]+h*vy[i-1];
 
     }
     }
     else{
         for(int i = 1; i < n; i++){
-            double r = sqrt(pow(rx[i],2)+pow(ry[i],2));
+            double r = sqrt(pow(rx[i-1],2)+pow(ry[i-1],2));
 
             float ax = -4*pow(M_PI, 2.0)*rx[i-1]/pow(r, 3.0);
             
-            rx[i] = rx[i-1]+h*vx[i]+pow(h,2.0)/2.0*ax; 
+            rx[i] = rx[i-1]+h*vx[i-1]+pow(h,2.0)/2.0*ax; 
             float ax1 = -4*pow(M_PI, 2.0)*rx[i]/pow(r, 3.0);          
             vx[i] = vx[i-1]+h*ax+pow(h,2.0)/2.0*((ax1-ax)/h);
             
             
             float ay = -4*pow(M_PI, 2.0)*ry[i-1]/pow(r, 3.0);
             
-            ry[i] = ry[i-1]+h*vy[i]+pow(h,2.0)/2.0*ay; 
+            ry[i] = ry[i-1]+h*vy[i-1]+pow(h,2.0)/2.0*ay; 
             float ay1 = -4*pow(M_PI, 2.0)*ry[i]/pow(r, 3.0);          
             vy[i] = vy[i-1]+h*ay+pow(h,2.0)/2.0*((ay1-ay)/h);
 
     }
     }
+    cout << "vx" <<" " << "vy" <<" " << "rx"<<" " << "ry" << endl;
     for(int i = 0; i< vy.size(); i++){
-        cout << "vx: " << vx[i] << endl;
-        cout << "vy: " << vy[i] << endl;
-        cout << "rx: " << rx[i] << endl;
-        cout << "ry " << ry[i] << endl;
+      cout << vx[i] << "     " << vy[i] <<"     " << rx[i]<<"     " << ry[i] << endl;
+        
         
     }   
         
+    myfile.open ("values3.txt");
     
+    for(int i = 0; i<n; i+=1){
+        myfile << rx[i] << " " << ry[i] << endl;
+    }
+    myfile.close();
+   
+    
+
+    return 0;
 
 }   
