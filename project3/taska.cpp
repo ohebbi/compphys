5a3b9432883ac8b270643e472f26b22c99cf63f5
Ingvild Bergsbak <ingbergs@uio.no>
2018-10-03 15:15:32 +0200

    task a

--- project3/taska.cpp
+++ project3/taska.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <string.h>
+#include <stdio.h>
 
 using namespace std;
 
 int main(int argc,char* argv[]){
+    int n = 10;
+  
+    vector<double> vx(n, 0);
+    vector<double> vy(n, 0);
+    vector<double> rx(n, 0);
+    vector<double> ry(n, 0);
 
-    vector<double> vx;
-    vector<double> vy;
-    vector<double> rx;
-    vector<double> ry;
-
-    vx.push_back(1/sqrt(2));
-    vy.push_back(1/sqrt(2));
-    rx.push_back(1);
-    ry.push_back(0);
+    vx[0]=(1/sqrt(2));
+    vy[0]=(1/sqrt(2));
+    rx[0]=(1);
+    ry[0]=(0);
 
     
-    int n = 100;
+    
     float h = 1./n;
 
-    if (argv[1]= "euler"){
+    if ( strncmp(argv[1], "e", 2)==0){
+      cout << "euler" << endl;
         for(int i = 1; i < n; i++){
-            double r = sqrt(pow(rx[i],2)+pow(ry[i],2));
+            float r = sqrt(pow(rx[i-1],2)+pow(ry[i-1],2));
 
-            vx[i] = vx[i-1]-h*4*pow(pi, 2.0)*rx[i-1]/pow(r,3);
-            rx[i] = rx[i-1]+h*vx[i];
+            vx[i] = vx[i-1]+h*4.0*pow(3.14159, 2.0)*rx[i-1]/pow(r,3.0);
+            rx[i] = rx[i-1]+h*vx[i-1];
 
-            vy[i] = vy[i-1]-h*4*pow(pi, 2.0)*ry[i-1]/pow(r,3);
-            ry[i] = ry[i-1]+h*vy[i];
+            vy[i] = vy[i-1]+h*4.0*pow(3.14159, 2.0)*ry[i-1]/pow(r,3.0);
+            ry[i] = ry[i-1]+h*vy[i-1];
 
     }
     }
@@ -36,21 +40,21 @@ int main(int argc,char* argv[]){
         for(int i = 1; i < n; i++){
             double r = sqrt(pow(rx[i],2)+pow(ry[i],2));
 
-            float ax = -4*pow(pi, 2.0)*rx[i-1]/pow(r, 3.0);
+            float ax = -4*pow(M_PI, 2.0)*rx[i-1]/pow(r, 3.0);
             
             rx[i] = rx[i-1]+h*vx[i]+pow(h,2.0)/2.0*ax; 
-            float ax1 = -4*pow(pi, 2.0)*rx[i]/pow(r, 3.0);          
+            float ax1 = -4*pow(M_PI, 2.0)*rx[i]/pow(r, 3.0);          
             vx[i] = vx[i-1]+h*ax+pow(h,2.0)/2.0*((ax1-ax)/h);
             
             
-            float ay = -4*pow(pi, 2.0)*ry[i-1]/pow(r, 3.0);
+            float ay = -4*pow(M_PI, 2.0)*ry[i-1]/pow(r, 3.0);
             
             ry[i] = ry[i-1]+h*vy[i]+pow(h,2.0)/2.0*ay; 
-            float ay1 = -4*pow(pi, 2.0)*ry[i]/pow(r, 3.0);          
+            float ay1 = -4*pow(M_PI, 2.0)*ry[i]/pow(r, 3.0);          
             vy[i] = vy[i-1]+h*ay+pow(h,2.0)/2.0*((ay1-ay)/h);
 
     }
-
+    }
     for(int i = 0; i< vy.size(); i++){
         cout << "vx: " << vx[i] << endl;
         cout << "vy: " << vy[i] << endl;
@@ -59,6 +63,6 @@ int main(int argc,char* argv[]){
         
     }   
         
-    }
+    
 
 }   
