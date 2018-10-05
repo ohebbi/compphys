8fdbc4314b13e234b6f518ff73e6ac3cb82e4b88
Ingvild Bergsbak <ingbergs@uio.no>
2018-10-05 14:20:26 +0200

    with correct initial values

--- project3/taskd.cpp
+++ project3/taskd.cpp
@@ -22,7 +22,7 @@ class Planet {
 
 
 tuple<vector<double>, vector<double>, vector<double>> bane(float final_time, vector<double> init){
-    int n = 1000;
+    int n = 100000;
     
     vector<double> vx(n, 0);
     vector<double> vy(n, 0);
@@ -30,8 +30,8 @@ tuple<vector<double>, vector<double>, vector<double>> bane(float final_time, vec
     vector<double> rx(n, 0);
     vector<double> ry(n, 0);
     vector<double> rz(n, 0);
-    vector<double> ax(n, 0);
-    vector<double> ay(n, 0);
+    //vector<double> ax(n, 0);
+    //vector<double> ay(n, 0);
 
     vx[0]=(init[0]);
     vy[0]=(init[1]);
@@ -43,31 +43,37 @@ tuple<vector<double>, vector<double>, vector<double>> bane(float final_time, vec
     float b = 3.;
     float h = final_time/n;
     double r;
+    r = sqrt(pow(rx[0],2)+pow(ry[0],2)+pow(rz[0],2));
+    float ax1 = -4*pow(M_PI, 2.0)*rx[0]/pow(r, b);
+    float ay1 = -4*pow(M_PI, 2.0)*ry[0]/pow(r, b);
+    float az1 = -4*pow(M_PI, 2.0)*rz[0]/pow(r, b);
+    
+    
     
     
     for(int i = 1; i < n; i++){
-        r = sqrt(pow(rx[i-1],2)+pow(ry[i-1],2)+pow(rz[i-1],2));
+        
       
-        float ax = -4*pow(M_PI, 2.0)*rx[i-1]/pow(r, b);
-        float ay = -4*pow(M_PI, 2.0)*ry[i-1]/pow(r, b);
-        float az = -4*pow(M_PI, 2.0)*rz[i-1]/pow(r, b);
+        float ax = ax1;
+        float ay = ay1;
+        float az = az1;
      
 
         rx[i] = rx[i-1]+h*vx[i-1]+pow(h,2.0)/2.0*ax;
 	ry[i] = ry[i-1]+h*vy[i-1]+pow(h,2.0)/2.0*ay;
 	rz[i] = rz[i-1]+h*vz[i-1]+(pow(h,2.0)/2.0)*az;
 	
-	//r = sqrt(pow(rx[i],2)+pow(ry[i],2)+pow(rz[i],2));
+	r = sqrt(pow(rx[i],2)+pow(ry[i],2)+pow(rz[i],2));
 	
-	float ax1 = -4*pow(M_PI, 2.0)*rx[i]/pow(r, b);
+	ax1 = -4*pow(M_PI, 2.0)*rx[i]/pow(r, b);
         vx[i] = vx[i-1]+(h/2.0)*(ax1+ax);
        
         
-        float ay1 = -4*pow(M_PI, 2.0)*ry[i]/pow(r, b);
+        ay1 = -4*pow(M_PI, 2.0)*ry[i]/pow(r, b);
         vy[i] = vy[i-1]+(h/2.0)*(ay1+ay);
 
         
-        float az1 = -4*pow(M_PI, 2.0)*rz[i]/pow(r, b);
+        az1 = -4*pow(M_PI, 2.0)*rz[i]/pow(r, b);
         vz[i] = vz[i-1]+(h/2.0)*(az1+az);
 
 
@@ -81,8 +87,9 @@ int main(int argc,char* argv[]){
     Planet earth;
     earth.mass = 1;
     earth.DS = 1;
-    earth.inv = {-1.1369, 6.172, 0.000365 , 0.987018, 0.1720378, -0.851238};
-    auto over = bane(2.625, earth.inv);
+    //earth.inv = {-1.1369, 6.172, 0.000365 , 0.987018, 0.1720378, -0.0000851238};
+    earth.inv = {0, 2*M_PI, 0 , 1, 0, 0};
+    auto over = bane(1.0, earth.inv);
     earth.orbit = over;
     
 
