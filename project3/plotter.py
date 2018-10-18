e93e2f26da4ed7079b924169b796b0cdae89543e
Ingvild Bergsbak <ingbergs@uio.no>
2018-10-18 10:15:23 +0200

    something

--- project3/plotter.py
+++ project3/plotter.py
@@ -5,20 +5,38 @@ import matplotlib.pyplot as plt
 import sys
 
 f = open("values3.txt", "r")
+x = []
+y = []
 rx = []
 ry = []
+jrx = []
+jry = []
 
 f1 = f.readlines()
 for i in f1:
     a = i.split(" ")
-    rx.append(float(a[0]))
-    ry.append(float(a[1]))
+    x.append(float(a[0]))
+    y.append(float(a[1]))
 
 f.close()
 
+
+
+for i in range(0, len(x)):
+    if(i%2==0):
+        rx.append(x[i])
+        ry.append(y[i])
+    else:
+        jrx.append(x[i])
+        jry.append(y[i])
+        
+    
 plt.plot(rx,ry)
+plt.plot(jrx,jry)
+
+
 plt.xlabel("rx")
 plt.ylabel("ry")
 plt.legend(['rx', 'ry'])
-plt.title("n=100000 for the general matrix")
+plt.title("Our solar system. What a beauty. :)")
 plt.show()
