58c7d22f88b5b84232a25aba8f71f07abb1256e8
Erlend Lemva Ousdal <erlendou@geigerteller.uio.no>
2018-11-01 13:20:28 +0100

    hola

--- project4/4a.cpp
+++ project4/4a.cpp
@@ -7,21 +7,13 @@
 
 using namespace std;
 
-double E_i(vector<int>x, vector<int>y){
-  double s=0;
-  for(int i=0; i<x.size(); i++){  
-    s-=2*x[i]*y[i];
-    if((i+1)>=x.size()){
-      s-=x[i]*x[0];
-    }
-    else{
-      s-=x[i]*x[i+1];
-    }
-    if((i+1)>=y.size()){
-      s-=y[i]*y[0];
-    }
-    else{
-      s-=y[i]*y[i+1];
+const int L = 2;
+
+double E_i(int m[L][L]){
+  double  s = 0;
+  for(int i =0; i < L; i++){
+    for(int j = 0; j < L; j++){
+      
     }
   }
   return s;
@@ -31,20 +23,18 @@ double mean_E(double Z, double sum_E){
   return (1/Z)*sum_E;
 }
 
-int main(){
+int main(){ 
   ofstream myfile;
-  myfile.open("plot.txt");
-  
-  int L = 2;
-  vector<int> x(L, 0);
-  vector<int> y(L, 0);
+  int m[L][L];
   vector<int>spin={-1, 1, -1, 1, -1, -1};
-  for(int i=0; i<x.size(); i++){
-    x[i]=spin[rand()%spin.size()];
-    y[i]=spin[rand()%spin.size()];
-    //cout << x[i] << "   " <<y[i] << endl;
+  for(int i=0; i<L; i++){
+    for(int j=0; j<L; j++){
+	m[i][j]=spin[rand()%spin.size()];
+	cout << m[i][j];
+     }
+
   }
-  double E_b = E_i(x, y);
+  double E_b = E_i(m);
   double sum_E = E_b*exp(-E_b);
   double Z = exp(-E_b);
   double r;
@@ -52,39 +42,40 @@ int main(){
   myfile << 0 << " " << mean_E(Z, sum_E) << " \n";
   
   
-
-for(int i=1; i<1000; i++){
-    vector<int> x1 = x;    vector<int> y1 = y;
+  
+for(int i=1; i<100; i++){
+    int (*m1)[L] = m;
     int a = rand()%2;
     if(spin[rand()%spin.size()]==1){
-      x1[a] = -x1[a];
-    }    else{
-      y1[a] = -y1[a];
+      m1[0][a] = -m1[0][a];
+    }   
+    else{
+      m1[1][a] = -m1[1][a];
     }
-    double E_new = E_i(x1, y1);
+    double E_new = E_i(m1);
     double delta_E = E_new - E_b;
     if(delta_E<=0){
-      x=x1;
-      y=y1;
-      sum_E += E_new*exp(-E_new);
-      Z += exp(-E_new);
-      E_b = E_new;
-      cout << mean_E(Z, sum_E)<< endl;
-    }
-    else{
-      r = (rand()%11)/10;
-      double w = exp(-delta_E);
-      if(w<=r){
-	x=x1;
-	y=y1;
-	sum_E += E_new*exp(-E_new);
-	Z += exp(-E_new);
-	E_b = E_new;
-	cout << mean_E(Z, sum_E)<< "yyyyyygh" << endl;
+       int (*m)[L]  = m1;
+       sum_E += E_new*exp(-E_new);
+       Z += exp(-E_new);
+       E_b = E_new;
+       cout << mean_E(Z, sum_E)<< endl;
+    }      
+      else{
+	r = (rand()%11)/10;
+	double w = exp(-delta_E);
+	if(w<=r){
+	     int (*m)[L]  = m1 ;
+             sum_E += E_new*exp(-E_new);
+	     Z += exp(-E_new);
+	     E_b = E_new;
+	     cout << mean_E(Z, sum_E)<< "yyyyyygh" << endl;
       }
-    }
-    myfile << i << " " << mean_E(Z, sum_E) << " \n";
-  }
-  
+      } 
+      
+      myfile << i << " " << mean_E(Z, sum_E) << " \n";	
+      
+      
+  }  
   return 0;
 }
