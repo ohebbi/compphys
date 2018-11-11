d8f4072723e97b9dd1c2633b2e3cb26ad2913775
Oliver <oliverlh@uio.no>
2018-11-11 13:30:58 +0100

    +random

--- project4/4e.cpp
+++ project4/4e.cpp
@@ -8,12 +8,19 @@
 
 using namespace std;
 
+
+const int L = 40;
+const int nspins = L*L;
+
+const int N = 1e6;
+
+
 double E;
 double T_init=2.0;
 double T_final=2.3;
 double delta_T=0.05;
 
-const int L = 20;
+
 
 double M_i(int m[L][L]){
   double M = 0;
@@ -55,27 +62,38 @@ double E_i(int m[L][L]){
 vector<double> mean_E_M(double Z, double sum_E, double sum_M, double sum_E_heatcap, double sum_E_suscept, double temp){
   vector<double> mean;
   mean.push_back((1/Z)*sum_E); // mean[0]
-  //mean.push_back((1/(pow(temp,2))*((1/Z)*sum_E_heatcap-pow((1/Z)*sum_E,2)))); // C_v : mean[1]
-  
-mean.push_back((1/Z)
-vector<double> mean_E_*sum_M); // mean[2]
-//mean.push_back((1/temp)*((((1/Z)*sum_E_suscept)-pow(mean[2],2)))); // susceptibility : mean[3]
+  mean.push_back((1/(pow(temp,2))*((1/Z)*sum_E_heatcap-pow((1/Z)*sum_E,2)))); // C_v : mean[1]
+  mean.push_back((1/Z)*sum_M); // mean[2]
+  mean.push_back((1/temp)*((((1/Z)*sum_E_suscept)-pow(mean[2],2)))); // susceptibility : mean[3]
   return mean;
 }
 
-
-
-
-
-
+int spin(double ixx){
+  if(ixx>=0.5){
+    return 1;
+  }
+  else{
+    return -1;
+  }
+}
 
 int main(int nargs, char* args[]){
-  int numprocs, my_rank; 
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_real_distribution<double> dis(0.0, 1.0);
+  uniform_real_distribution<double> dist(0.0, L);
+
+  double ix;
+  int a;
+  int b;
+  double E_new;
+
+  int numprocs, my_rank;
   double  time_start, time_end, total_time;
-  
+
   ofstream myfile;
   myfile.open("plot1.txt");
-  
+
   MPI_Init (&nargs, &args);
   MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
@@ -87,84 +105,82 @@ int main(int nargs, char* args[]){
   double n=(T_final-T_init)/(delta_T*numprocs);
 
   int m[L][L];
-  vector<int>spin={1, -1, -1, -1, 1, -1};
-
-  for(int i=0; i<=n; i++){
-
   for(int i=0; i<L; i++){
     for(int j=0; j<L; j++){
-	     m[i][j]=spin[rand()%spin.size()];
+      ix = dis(gen);
+      m[i][j]=spin(ix);
     }
   }
 
-  double temp = 1.0; //temperature
-  
+  for(int i=0; i<=n; i++){
+
   double E_b = -E_i(m);
-  double B = exp(-E_b/temp);
+  double B = exp(-E_b/T_0);
   double sum_E = E_b*B;
   double sum_M = M_i(m)*B;
   double sum_E_heatcap = pow(E_b,2)*B;
   double sum_E_suscept = pow(M_i(m),2)*B;
 
-  double Z = exp(-E_b/temp);
+  double Z = exp(-E_b/T_0);
   double r;
-  vector<double> mean = mean_E_M(Z, sum_E, sum_M, sum_E_heatcap, sum_E_suscept, temp);
-    myfile << 0 << " " << mean[0] << " " << mean[1] << " " << mean[2] << " " << mean[3] <<  " \n";
+  vector<double> mean = mean_E_M(Z, sum_E, sum_M, sum_E_heatcap, sum_E_suscept, T_0);
+  myfile << T_0 << " " << i << " " << mean[0] << " " << mean[1]  << " " << mean[2] << " " << mean[3] <<  " \n";
 
 
-  for(int i=1; i<1e5; i++){
+  for(int i=1; i<N; i++){
 
     int (*m1)[L] = m;
-    int a = rand()%L;
-    int b = rand()%L;
+    a = (int)dist(gen);
+    b = (int)dist(gen);
 
     m1[b][a] = -m1[b][a];
 
-    double E_new = -E_i(m1);
+    E_new = -E_i(m1);
     double delta_E = E_new - E_b;
     if(delta_E<=0){
-       B=exp(-E_new/temp);
+       B=exp(-E_new/T_0);
        int (*m)[L] = m1;
        sum_E += E_new*B;
        Z     += B;
        sum_M += M_i(m)*B;
-       //sum_E_heatcap += pow(E_new,2) *B;
-       //sum_E_suscept += pow(M_i(m1),2)*B;
+       sum_E_heatcap += pow(E_new,2) *B;
+       sum_E_suscept += pow(M_i(m1),2)*B;
        E_b    = E_new;
-       mean   = mean_E_M(Z, sum_E, sum_M, sum_E_heatcap, sum_E_suscept, temp);
+       mean   = mean_E_M(Z, sum_E, sum_M, sum_E_heatcap, sum_E_suscept, T_0);
 
     }
     else{
-       r = (rand()%11)/10.0;
+       r = dis(gen);
        double w = exp(-delta_E);
-       B=exp(-E_new/temp);
+       B=exp(-E_new/T_0);
        if(w<=r){
-	 int (*m)[L]  = m1 ;
-	 sum_E += E_new*B;
-	 Z     += B;
-	 sum_M += M_i(m)*B;
-	 //sum_E_heatcap += pow(E_new,2) *B;
-       	 //sum_E_suscept += pow(M_i(m1),2)*B;
-	 E_b    = E_new;
-	 mean   = mean_E_M(Z, sum_E, sum_M, sum_E_heatcap, sum_E_suscept, temp);
+	        int (*m)[L]  = m1 ;
+	        sum_E += E_new*B;
+	        Z     += B;
+	        sum_M += M_i(m)*B;
+	        sum_E_heatcap += pow(E_new,2) *B;
+          sum_E_suscept += pow(M_i(m1),2)*B;
+	        E_b    = E_new;
+	        mean   = mean_E_M(Z, sum_E, sum_M, sum_E_heatcap, sum_E_suscept, T_0);
        }
     }
+    if(float(i)==(N)){
+      myfile << T_0 << " " << i << " " << mean[0]/nspins << " " << mean[1]/nspins << " " << mean[2]/nspins << " " << mean[3]/nspins <<" \n";
 
-    myfile << T_0 << " " << mean[0] << " " << mean[1] << " " << mean[2] << " " << mean[3] << i <<" \n";
-    T_0+=delta_T;
+    }
+
   }
-
+  T_0+=delta_T;
  }
-  //  cout << mean[0] << " " << mean[1] << " " << mean[2] << " " << mean[3] << my_rank << " \n";
+ //cout << T_0 << mean[0] << " " << mean[1] << i << " " << mean[2] << " " << mean[3] << my_rank << " \n";
  myfile.close();
 
 
-  
+
   time_end = MPI_Wtime();
   total_time = time_end-time_start;
 
-  MPI_Finalize (); 
+  MPI_Finalize ();
   return 0;
-  
-}
 
+}
