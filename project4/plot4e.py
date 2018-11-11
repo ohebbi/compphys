8a63810a1b595bf72d40c8a21b8d27692eee99d8
Ingvild Bergsbak <ingbergs@uio.no>
2018-11-11 19:48:19 +0100

    hhh

--- project4/plot4e.py
+++ project4/plot4e.py
@@ -3,17 +3,56 @@ import matplotlib.pyplot as plt
 
 e=[]
 t=[]
-f = open("plot1.txt", "r")
-f1 = f.readlines()
-del f1[-1]
-for i in f1:
+cv=[]
+m=[]
+x=[]
+
+f1 = open("L40-1.txt", "r")
+f11 = f1.readlines()
+f2 = open("L40-2.txt", "r")
+f22 = f2.readlines()
+
+for i in f11:
     a = i.split(" ")
     t.append(float(a[0]))
     e.append(float(a[2]))
+    cv.append(float(a[3]))
+    m.append(float(a[4]))
+    x.append(float(a[5]))
+for i in f22:
+    a = i.split(" ")
+    t.append(float(a[0]))
+    e.append(float(a[2]))
+    cv.append(float(a[3]))
+    m.append(float(a[4]))
+    x.append(float(a[5]))
 
-f.close()
+f1.close()
+f2.close()
 
-
+plt.subplot(2, 2, 1)
 plt.plot(t,e)
+plt.legend(["$<E>$"])
+plt.xlabel("Temperature")
+plt.ylabel("Energy")
+
+plt.subplot(2, 2, 2)
+plt.plot(t,cv)
+plt.legend(["$C_V$"])
+plt.xlabel("Temperature")
+plt.ylabel("Heat capacity")
+
+plt.subplot(2, 2, 3)
+plt.plot(t,m)
+plt.legend(["<M>"])
+plt.xlabel("Temperature")
+plt.ylabel("Magnetization")
+
+plt.subplot(2, 2, 4)
+plt.plot(t,x)
+plt.legend(["$\chi$"])
+plt.xlabel("Temperature")
+plt.ylabel("Susceptibility")
+
 plt.show()
 
