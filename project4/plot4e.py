24aa09d5933c079424c1d3031475709feeab3b80
Harald Bergsbak <Harald@Haralds-MacBook-Pro.local>
2018-11-12 20:58:39 +0100

    plotter

--- project4/plot4e.py
+++ project4/plot4e.py
@@ -6,27 +6,45 @@ t=[]
 cv=[]
 m=[]
 x=[]
+L=100
 
-f1 = open("L40-1.txt", "r")
+f1 = open("L%s-1.txt" %L, "r")
 f11 = f1.readlines()
-f2 = open("L40-2.txt", "r")
+f2 = open("L%s-2.txt" %L, "r")
 f22 = f2.readlines()
+f3 = open("L%s-3.txt" %L, "r")
+f33 = f3.readlines()
+f4 = open("L%s-4.txt" %L, "r")
+f44 = f4.readlines()
 
 for i in f11:
     a = i.split(" ")
     t.append(float(a[0]))
-    e.append(float(a[2]))
-    cv.append(float(a[3]))
-    m.append(float(a[4]))
-    x.append(float(a[5]))
+    e.append(float(a[1]))
+    cv.append(float(a[2]))
+    m.append(float(a[3]))
+    x.append(float(a[4]))
 for i in f22:
     a = i.split(" ")
     t.append(float(a[0]))
-    e.append(float(a[2]))
-    cv.append(float(a[3]))
-    m.append(float(a[4]))
-    x.append(float(a[5]))
-
+    e.append(float(a[1]))
+    cv.append(float(a[2]))
+    m.append(float(a[3]))
+    x.append(float(a[4]))
+for i in f33:
+    a = i.split(" ")
+    t.append(float(a[0]))
+    e.append(float(a[1]))
+    cv.append(float(a[2]))
+    m.append(float(a[3]))
+    x.append(float(a[4]))
+for i in f44:
+    a = i.split(" ")
+    t.append(float(a[0]))
+    e.append(float(a[1]))
+    cv.append(float(a[2]))
+    m.append(float(a[3]))
+    x.append(float(a[4]))
 f1.close()
 f2.close()
 
@@ -54,6 +72,6 @@ plt.legend(["$\chi$"])
 plt.xlabel("Temperature")
 plt.ylabel("Susceptibility")
 
-plt.savefig("4eL40.pdf")
+plt.savefig("4eL%i.pdf" %L)
 plt.show()
 
