3d377b30ff9e24fea4680c2cc901adc6cc3c5649
Erlend Lemva Ousdal <er_lendo@hotmail.com>
2018-11-15 11:01:06 +0100

    plotter

--- project4/plotting4b.py
+++ project4/plotting4b.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from scipy import stats  
 
 f = open("plot.txt", "r")
 a = []
@@ -12,45 +14,58 @@ E=-7.9836/4
 Cv=0.1283/4
 X=15.973/4
 
-while len(a)<0.8e5:
+while len(n)<1.0e6:
     i = f.readline()
-    allah = i.split(" ")
-    n.append(float(allah[0]))
-    a.append(float(allah[1]))
-    b.append(float(allah[2]))
-    c.append(float(allah[3]))
-    d.append(float(allah[4]))
+   
+    
+    n.append(float(i))
+    #a.append(float(allah[1]))
+    #b.append(float(allah[2]))
+    # c.append(float(allah[3]))
+    #d.append(float(allah[4]))
 
 
+x = np.linspace(min(n)-4,max(n)+4, 40)
+y = np.exp(-((x-(-1.24))**2)/(2*8.11126))*1.0/(np.sqrt(2*np.pi*8.11126))
+
+plt.hist(n, normed = 1, bins = 23)
+plt.plot(x,y)
+plt.xlabel("Energy (1/J)")
+plt.ylabel("Proabability (%)")
+plt.legend(["probability density","nummerical values"])
 f.close()
+"""
+plt.subplot(1, 2, 1)
+plt.plot(n[2:],b[2:])
+plt.legend(["$\sigma^2$"])
+plt.xlabel("Monte Carlo cycles")
+plt.ylabel("Variance in energy")
 
-plt.subplot(2, 2, 1)
-plt.plot(n[:1000],a[:1000])
-plt.legend(["<E>"])
-plt.xlabel("Number of Monte Carlo cycles")
-plt.ylabel("Mean energy")
 
 
-plt.subplot(2, 2, 2)
-plt.plot(n[:1000],b[:1000])
-#plt.legend(["$C_V$"])
-plt.xlabel("Number of Monte Carlo cycles")
-plt.ylabel("Heat capacity")
+plt.subplot(1, 2, 2)
+plt.plot(n[-100:],b[-100:])
+plt.legend(["$\sigma^2$"])
+plt.xlabel("Monte Carlo cycles")
+plt.ylabel("Variance in energy")
 
-plt.subplot(2, 2, 3)
-plt.plot(n[:1000],c[:1000])
-plt.legend(["<M>"])
+plt.subplot(1, 2, 2)
+plt.plot(n,c)
+plt.legend(["<|M|>"])
 plt.xlabel("Number of Monte Carlo cycles")
 plt.ylabel("Mean magnetization")
 
 plt.subplot(2, 2, 4)
-plt.plot(n[:1000],d[:1000])
+plt.plot(n,d)
 plt.legend(["$\chi$"])
 plt.xlabel("Number of Monte Carlo cycles")
 plt.ylabel("Susceptibility")
 
+"""
 
-plt.tight_layout()
+
+
+plt.tight_layout(pad=0.4, w_pad=1.5, h_pad=1.0)
 
 """
 plt.xlabel("Number of Monte-Carlo cycles")
@@ -70,5 +85,6 @@ plt.xlabel("Energy (J)")
 plt.ylabel("Normalized probability")
 plt.savefig("paral.pdf")
 """
-#plt.savefig("allup.pdf")
+plt.savefig("L20prob24.pdf")
 plt.show()
+
